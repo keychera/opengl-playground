c8d85624d29062e9b127f2803a308e2adb87586e
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2020-08-29 17:35:00 +0700

    "hello window"

--- hellogl.cpp
+++ hellogl.cpp
@@ -3,7 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 int main()
 {
@@ -31,12 +32,23 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
