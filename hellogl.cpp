6e533df0d2a873cecd718bc6d8218dd9627c663e
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2020-08-30 09:29:30 +0700

    hello shaders, uniforms!

--- hellogl.cpp
+++ hellogl.cpp
@@ -1,30 +1,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
 
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "out vec4 vertexColor;\n"
                                  "void main()\n"
                                  "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
+                                 "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
                                  "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
+                                   "in vec4 vertexColor;"
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "    FragColor = vec4(vertexColor.rg, 1.0f, 1.0f);\n"
                                    "}\n";
 
 const char *fragmentShaderSource2 = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "    FragColor = vec4(0.5f, 1.0f, 0.2f, 1.0f);\n"
-                                   "}\n";
+                                    "out vec4 FragColor;\n"
+                                    "uniform vec4 ourColor;\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "    FragColor = ourColor;\n"
+                                    "}\n";
 
 int main()
 {
@@ -47,7 +52,6 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
     glViewport(0, 0, 800, 600);
 
     // compile vertex and fragmentShader first
@@ -168,7 +172,12 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram2, "ourColor");
         glUseProgram(shaderProgram2);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(VAO2);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
