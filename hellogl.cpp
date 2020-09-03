1d92d79d372768826a20747fb3d8a4effb683b80
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2020-09-03 09:24:49 +0700

    ten cube elegantly rotatin

--- hellogl.cpp
+++ hellogl.cpp
@@ -96,6 +96,18 @@ int main()
         -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
+
     unsigned int VBO, VAO;
     unsigned int VBO2, VAO2;
     unsigned int cubeVBO, cubeVAO;
@@ -264,9 +276,20 @@ int main()
         cubeShader.setFloat("mixVal", mixVal);
         cubeShader.setFloat("zoom", zoomVal);
         glBindVertexArray(cubeVAO);
-        model = glm::rotate(model, (float)glfwGetTime() * 0.001f * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        modelLoc = glGetUniformLocation(cubeShader.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            model = glm::mat4(1.0f);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, (float)timeValue * glm::radians(50.0f), glm::vec3(0.5f, sin(timeValue) * 0.5f, 0.0f));
+            model = glm::translate(model, cubePositions[i]);
+            modelLoc = glGetUniformLocation(cubeShader.ID, "model");
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
