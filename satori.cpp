b5e69a749ddec19e782af99ae110a13acf50cc83
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2020-09-14 16:45:27 +0700

    toon shader

--- satori.cpp
+++ satori.cpp
@@ -33,8 +33,11 @@ float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
+// control mode
+unsigned int mode = 1;
+
 //light
-glm::vec3 lightPos(0.7f, 0.7f, 1.0f);
+glm::vec3 lightPos(0.2f, 0.1f, 2.5f);
 
 int main()
 {
@@ -52,8 +55,8 @@ int main()
   }
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-  glfwSetCursorPosCallback(window, mouse_callback);
+  // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  // glfwSetCursorPosCallback(window, mouse_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
@@ -63,21 +66,21 @@ int main()
   glViewport(0, 0, 800, 600);
 
   // compile vertex and fragmentShader first
-  Shader squareShader(SHADERS_LOC "/cube.vs", SHADERS_LOC "/satori.fs");
+  Shader squareShader(SHADERS_LOC "/satori.vs", SHADERS_LOC "/satori.fs");
   Shader lightCubeShader(SHADERS_LOC "/light_cube.vs", SHADERS_LOC "/light_cube.fs");
 
   // preparing datas to draw
   float cube_vertices[] = {
-      // positions          // normals           // texture coords
-      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-      0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
-      0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-      0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-      -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // for formatting
+      // positions          // texture coords
+      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+      0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+      0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+      0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+      -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // for formatting
   };
 
-  int numOfVertexData = 6;
+  int numOfVertexData = 5;
   int numOfTriangles = sizeof(cube_vertices) / (sizeof(cube_vertices[0]) * numOfVertexData);
 
   unsigned int cubeVBO, cubeVAO;
@@ -90,15 +93,12 @@ int main()
   glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
 
   // position attribute
-  int blockSize = 8;
+  int blockSize = 5;
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, blockSize * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
-  // normal attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, blockSize * sizeof(float), (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
   // tex attribute
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, blockSize * sizeof(float), (void *)(6 * sizeof(float)));
-  glEnableVertexAttribArray(2);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, blockSize * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // light VAO
   unsigned int lightCubeVAO;
@@ -114,11 +114,13 @@ int main()
   // -----------------------------------------------------------------------------
   stbi_set_flip_vertically_on_load(true);
   unsigned int diffuseMap = loadTexture(ASSETS_LOC "/sprite_0093.png");
+  unsigned int normalMap = loadTexture(ASSETS_LOC "/sprite_0093_n.png");
 
   // shader configuration
   // --------------------
   squareShader.use();
   squareShader.setInt("material.diffuse", 0);
+  squareShader.setInt("material.normal", 1);
 
   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -158,9 +160,12 @@ int main()
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, diffuseMap);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, normalMap);
 
     glBindVertexArray(cubeVAO);
     model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 2.0f));
     squareShader.setMat4("model", model);
     glDrawArrays(GL_TRIANGLES, 0, numOfTriangles);
 
@@ -171,7 +176,7 @@ int main()
 
     model = glm::mat4(1.0f);
     model = glm::translate(model, lightPos);
-    model = glm::scale(model, glm::vec3(0.05f));
+    model = glm::scale(model, glm::vec3(0.01f));
     model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     lightCubeShader.setMat4("model", model);
 
@@ -214,18 +219,46 @@ void processInput(GLFWwindow *window)
     zoomVal -= 0.01f;
   }
 
-  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    camera.ProcessKeyboard(FORWARD, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    camera.ProcessKeyboard(BACKWARD, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    camera.ProcessKeyboard(LEFT, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    camera.ProcessKeyboard(RIGHT, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-    camera.ProcessKeyboard(ASCEND, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
-    camera.ProcessKeyboard(DESCEND, deltaTime);
+  // switching control mode
+  if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+  {
+    mode = 1;
+    camera.Position = glm::vec3(0.0f, 0.0f, 3.0f);
+  }
+  if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+  {
+    mode = 2;
+    lightPos = glm::vec3(0.2f, 0.1f, 2.5f);
+  }
+
+  float speed = 0.3f * deltaTime;
+  switch (mode)
+  {
+  case 1:
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+      lightPos += glm::vec3(0.0f, speed, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+      lightPos += glm::vec3(0.0f, -speed, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+      lightPos += glm::vec3(-speed, 0.0f, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+      lightPos += glm::vec3(speed, 0.0f, 0.0f);
+    break;
+  default:
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+      camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+      camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+      camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+      camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+      camera.ProcessKeyboard(ASCEND, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+      camera.ProcessKeyboard(DESCEND, deltaTime);
+    break;
+  }
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
--- shaders/satori.fs
+++ shaders/satori.fs
@@ -1,7 +1,6 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;
 in vec3 FragPos;
 in vec2 TexCoords;
 
@@ -21,21 +20,38 @@ uniform Light light;
 
 struct Material {
   sampler2D diffuse;
+  sampler2D normal;
 };
 uniform Material material;
 
 void main() {
-  vec3 lightDir = normalize(light.position - FragPos);
+  vec3 modLightPos = vec3(light.position.xy, FragPos.z);
+  vec3 lightDir = normalize(modLightPos - FragPos);
 
   // ambient
-  vec4 ambient = vec4(light.ambient, 0.0) * texture(material.diffuse, TexCoords);
+  vec4 ambient =
+      vec4(light.ambient, 0.0) * texture(material.diffuse, TexCoords);
 
-  // diffuse
-  vec3 norm = normalize(Normal);
+  // diffuse + normal
+  vec3 normal = texture(material.normal, TexCoords).rgb;
+  vec3 norm = normalize(normal * 2.0 - 1.0);
   float diff = max(dot(norm, lightDir), 0.0);
-  vec4 diffuse =
-      vec4(light.diffuse, 1.0) * diff * texture(material.diffuse, TexCoords);
 
-  vec4 result = ambient + diffuse;
+  // toon
+  vec3 color;
+  if (diff > 0.95)
+    color = vec3(1.0, 0.5, 0.5);
+  else if (diff > 0.5)
+    color = vec3(0.6, 0.3, 0.3);
+  else if (diff > 0.25)
+    color = vec3(0.4, 0.2, 0.2);
+  else
+    color = vec3(0.2, 0.1, 0.1);
+
+  // vec4 diffuse =
+  // vec4(light.diffuse, 1.0) * diff * texture(material.diffuse, TexCoords);
+
+  float a = texture(material.diffuse, TexCoords).a;
+  vec4 result = ambient + vec4(color, a);
   FragColor = result;
 }
